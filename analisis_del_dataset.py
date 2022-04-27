e6dd2ab8a5c5ae1b183c4435c71be0802c95af72
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-27 20:14:29 +0200

    prueba 1

--- analisis_del_dataset.py
+++ analisis_del_dataset.py
@@ -3,39 +3,25 @@ import csv
 
 #funcion para analizar critica_pelis.csv 
 def analisis_critica_peliculas(archivo):
-    with open('critica_pelis.csv', newline=" ") as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar=',',
+    with open('critica_pelis.csv', newline=" ") as File:
+        reader = csv.reader(File, delimiter=',', quotechar=',',
                         quoting=csv.QUOTE_MINIMAL)
-        contador = 0
-        contador_critica = 0
-        contador_buena = 0
-        contador_mala = 0
-        contador_total = 0
+        
         for row in reader:
-            contador += 1
-            if row[2] == 'Critica':
-                contador_critica += 1
-            elif row[2] == 'Buena':
-                contador_buena += 1
-            elif row[2] == 'Mala':
-                contador_mala += 1
-            contador_total += 1
-        print('El 68% de los datos son criticas: ', contador_critica/contador*100)
-        print('El 95% de los datos son criticas: ', contador_critica/contador*100)
-        print('El 68% de los datos son buenas: ', contador_buena/contador*100)
-        print('El 95% de los datos son buenas: ', contador_buena/contador*100)
-        print('El 68% de los datos son malas: ', contador_mala/contador*100)
-        print('El 95% de los datos son malas: ', contador_mala/contador*100)
-        print('El 68% de los datos son criticas: ', contador_critica/contador*100)
-        print('El 95% de los datos son criticas: ', contador_critica/contador*100)
-        print('El 68% de los datos son buenas: ', contador_buena/contador*100)
-        print('El 95% de los datos son buenas: ', contador_buena/contador*100)
-        print('El 68% de los datos son malas: ', contador_mala/contador*100)
-        print('El 95% de los datos son malas: ', contador_mala/contador*100)
+            print(row)
+    
 
 
 
-
+nombre_archivo= "critica_pelis.csv"
+with open('critica_pelis.csv', "r") as archivo:
+    for linea in archivo:
+        linea= linea.rstrip()
+        separate= ","
+        lista = linea.split(",")
+        nota=int(lista[0])
+        numero_votantes= int (lista[1])
+        
 
 
 
