02b94952d23a748a3954b12e0384803169ee67f2
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-27 20:34:07 +0200

    añado ptro archivo csv que es el que va a analizar mi código y hago prueba 2

--- analisis_del_dataset.py
+++ analisis_del_dataset.py
@@ -1,27 +1,72 @@
 import csv
 
 
-#funcion para analizar critica_pelis.csv 
-def analisis_critica_peliculas(archivo):
-    with open('critica_pelis.csv', newline=" ") as File:
-        reader = csv.reader(File, delimiter=',', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
-        
-        for row in reader:
-            print(row)
-    
 
-
-
-nombre_archivo= "critica_pelis.csv"
-with open('critica_pelis.csv', "r") as archivo:
-    for linea in archivo:
-        linea= linea.rstrip()
-        separate= ","
-        lista = linea.split(",")
-        nota=int(lista[0])
-        numero_votantes= int (lista[1])
+#nombre_archivo= "critica_pelis.csv"
+#with open('critica_pelis.csv', "r") as archivo:
+#    for linea in archivo:
+      #  linea= linea.rstrip()
+    #    separate= ","
+     #   lista = linea.split(",")
+     #   nota=int(lista[0])
+     #   numero_votantes= int (lista[1])
         
 
 
 
+#función para analizar datos_peliculas.csv
+def analisis_datos_peliculas(nombre_archivo):
+    with open("datos_peliculas.csv", "r") as archivo:
+        for linea in archivo:
+            linea= linea.rstrip()
+            separate= ","
+            lista = linea.split(",")
+            nota=int(lista[0])
+            numero_votantes= int (lista[1])
+            promedio= nota/numero_votantes
+            print("La nota es: ", nota)
+            print("El numero de votantes es: ", numero_votantes)
+            print("El promedio es: ", promedio)
+            print("\n")
+    #obtenemos el 68% de los votos
+    with open("datos_peliculas.csv", "r") as archivo:
+        for linea in archivo:
+            linea= linea.rstrip()
+            separate= ","
+            lista = linea.split(",")
+            nota=int(lista[0])
+            numero_votantes= int (lista[1])
+            promedio= nota/numero_votantes
+            if promedio>=6.8:
+                print("La pelicula es buena")
+            else:
+                print("La pelicula es mala")
+            print("\n")
+    #obtenemos el 95% de los votos
+    with open("datos_peliculas.csv", "r") as archivo:
+        for linea in archivo:
+            linea= linea.rstrip()
+            separate= ","
+            lista = linea.split(",")
+            nota=int(lista[0])
+            numero_votantes= int (lista[1])
+            promedio= nota/numero_votantes
+            if promedio>=9.5:
+                print("La pelicula es buena")
+            else:
+                print("La pelicula es mala")
+            print("\n")
+    #obtenemos el 97% de los votos
+    with open("datos_peliculas.csv", "r") as archivo:
+        for linea in archivo:
+            linea= linea.rstrip()
+            separate= ","
+            lista = linea.split(",")
+            nota=int(lista[0])
+            numero_votantes= int (lista[1])
+            promedio= nota/numero_votantes
+            if promedio>=9.7:
+                print("La pelicula es buena")
+            else:
+                print("La pelicula es mala")
+            print("\n")
