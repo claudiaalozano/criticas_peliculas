3c9283a2c10030bec9603a7aca4fe6d6cc1e9be8
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-05-04 08:37:46 +0000

    más cambios en el main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from Clases import analisis_del_dataset 
-from Clases.analisis_del_dataset import *
+
 if __name__ == "__main__":
-  from Clases import analisis_del_dataset 
   from Clases.analisis_del_dataset import *
   media_notas_pandas()
   desviacion_estandar_pandas()
